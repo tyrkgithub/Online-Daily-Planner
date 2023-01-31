ee7c53b5db04b6e87f9f2949f74837ca4d3c842b
tyrkgitgub <tiayorke@hotmail.co.uk>
2023-01-31 22:29:34 +0000

    added code to retrive from local storage

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 var today = moment();
 $("#currentDay").text(today.format("Do MMM YYYY HH:mm"));
 
-// let currentHour = today.getHours();
+// Find the current hour
+const d = new Date();
+let currentHour = d.getHours();
 
+// Set the working hours for the schedule.
 let workingHours = [
   {
-    name: "9AM",
-    value: 9,
+    name: "9AM", // What I want to display on the page
+    value: 9, // The number to use to compare schedule time and current time
   },
   {
     name: "10AM",
@@ -44,98 +47,58 @@ let workingHours = [
 ];
 
 function createTable() {
-  let table = document.createElement("table");
-  $("table").addClass(".time-block");
-  let tableRow = document.createElement("tr");
-  $("tr").addClass(".row");
-  let th1 = document.createElement("th");
-  th1.innerHTML = "Time";
-  let th2 = document.createElement("th");
-  th2.innerHTML = "Schedule";
-  let th3 = document.createElement("th");
-  th3.innerHTML = "Save";
-  tableRow.append(th1, th2, th3);
-  table.append(tableRow);
+  // Does the following:
+
+  let table = $("<table>"); // Creates table
+  table.addClass("time-block"); // Gives table class
+
   for (let i = 0; i < workingHours.length; i++) {
-    let tableRow = document.createElement("tr");
-    $("tr").addClass(".row");
-    let amPM = document.createElement("td");
-    amPM.innerHTML = workingHours[i].name;
-    let userInput = document.createElement("textarea");
-    let button = document.createElement("button");
-    $("button").addClass(".saveBtn");
-    tableRow.append(amPM, userInput, button);
-    table.append(tableRow);
+    // For however many working hours there are do this:
+    let tableRow = $("<tr>"); // Create a table row
+    tableRow.addClass("row"); // Gives row class
+    tableRow.attr("id", workingHours[i].name);
+
+    let hour = $("<th>"); // Creates element in the row to display time
+    hour.addClass("hour"); // Give time a class
+    hour.textContent = workingHours[i].name; // Sets the workingHours.name strings as times
+
+    let description = $("<textarea>"); // Creates a text input area
+    description.addClass("textarea"); // Adds class to textarea
+
+    let button = $("<button>"); // Creates button
+    button.addClass("saveBtn"); // Adds class to button
+
+    tableRow.append(hour, description, button); // Joins the time, textarea and button columns in one row
+    table.append(tableRow); // Joins the rows to the table
   }
-  document.getElementById("timeCon").append(table);
-
-  button.addEventListener("click", function(event)
-  event.preventDefualt())
+  $("#timeCon").append(table); //Joins the table to the HTML
 }
-
 createTable();
 
-// let hour9 = moment().hour(9);
-// hour9.innerHTML = "9AM";
-// let hour10 = moment().hour(10);
-// hour10.innerHTML = "10AM";
-// let hour11 = moment().hour(11);
-// hour11.innerHTML = "11AM";
-// let hour12 = moment().hour(12);
-// hour12.innerHTML = "12PM";
-// let hour13 = moment().hour(13);
-// hour13.innerHTML = "1PM";
-// let hour14 = moment().hour(14);
-// hour14.innerHTML = "2PM";
-// let hour15 = moment().hour(15);
-// hour15.innerHTML = "3PM";
-// let hour16 = moment().hour(16);
-// hour16.innerHTML = "4PM";
-// let hour17 = moment().hour(17);
-// hour17.innerHTML = "5PM";
+$("#9AM .textarea").val(localStorage.getItem("9AM"));
+$("#10AM .textarea").val(localStorage.getItem("10AM"));
+$("#11AM .textarea").val(localStorage.getItem("11AM"));
+$("#12PM .textarea").val(localStorage.getItem("12PM"));
+$("#1PM .textarea").val(localStorage.getItem("1PM"));
+$("#2PM .textarea").val(localStorage.getItem("2PM"));
+$("#3PM .textarea").val(localStorage.getItem("3PM"));
+$("#4PM .textarea").val(localStorage.getItem("4PM"));
+$("#5PM .textarea").val(localStorage.getItem("5PM"));
 
-// workingHours.forEach(function (workingHours) {
-//   let amPM = workingHours.timeName;
-//   let timeId = workingHours.timeNum;
+$("button").on("click", function () {
+  let key = $(this).parent().attr("id");
+  console.log(key);
+  let value = $(this).siblings(".textarea").val();
+  console.log(value);
+  localStorage.setItem(key, value);
+});
 
-//   let row = $("<section>").addClass("row");
-//   timeCon.append(row);
-
-//   let span = $("<span>" + amPM + "</span>").addClass("hour col-1");
-//   row.append(span);
-
-//   let userInput = $("<textarea").addClass("col-10 textarea");
-//   userInput.attr("id", timeId);
-//   row.append(userInput);
-
-//   if (currentHour > timeId) {
-//     userInput.addClass("past");
-//     userInput.prop("disabled", true);
-//   } else if (currentHour < timeId) {
-//     userInput.addClass("future");
-//   } else if (currentHour === timeId) {
-//     userInput.addClass("present");
+// for (let i = 0; i < workingHours.length; i++) {
+//   if (currentHour > workingHours[i].value) {
+//     description.addClass("past");
+//   } else if (currentHour < workingHours[i].value) {
+//     description.addClass("future");
+//   } else if (currentHour === workingHours[i].value) {
+//     description.addClass("present");
 //   }
-//   var button = $("<button>").addClass("saveBtn col-1");
-//   row.append(button);
-//   var btnImg = $("<i>").addClass("fas fa-save fa-2x");
-//   button.append(btnImg);
-
-//   var key = timeId;
-//   var value = localStorage.getItem(key);
-//   userInput.val(value);
-
-//   row.on("click", "button", function (event) {
-//     event.preventDefault();
-//     var value = userInput.val();
-//     localStorage.setItem(key, value);
-//   });
-// });
-
-// let table =
-
-// var button = $("<button>").addClass("saveBtn col-1");
-// row.append(button);
-
-// var btnImg = $("<i>").addClass("fas fa-save fa-2x");
-// button.append(btnImg);
+// }
